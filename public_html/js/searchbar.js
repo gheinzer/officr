05527f519d09414f76833bc0c3095c3b72cc36ec
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-07-28 12:47:56 +0200

    Created public_html/js/searchbar.js

---
+++ public_html/js/searchbar.js
@@ -0,0 +1,10 @@
+function hideSearch() {
+  var searchbar = document.getElementById("searchBar");
+  searchbar.style.opacity = 0;
+  searchbar.style.pointerEvents = "none";
+}
+function showSearch() {
+  var searchbar = document.getElementById("searchBar");
+  searchbar.style.opacity = 1;
+  searchbar.style.pointerEvents = "all";
+}

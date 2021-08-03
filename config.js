b53497dc78790d4d02ad159800dd57e84f884170
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-08-03 21:20:38 +0200

    Set config.js to the default values

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
  * This is the configuration for the server that servers the files in public_html.
  */
 const httpd_config = {
-    port: 8001, // Sets the port for the server. Default: 80
-    lang: null, // Sets the language for the documents. When set to null, the default language of the client is used. Default: undefined
+    port: 80, // Sets the port for the server. Default: 80
+    lang: null, // Sets the language for the documents. When set to null, the default language of the client is used. Default: null
     logging: {
         // Configures, what is logged by the server.
         remote_lang: true, // Configures, wheter the client language is logged or not. Default: true

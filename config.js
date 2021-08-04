f0c621ed37c5e2307e57d5cf2f8b432ef7373ab5
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-08-04 17:05:47 +0200

    Configurated the server for testing purposes

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
  * This is the configuration for the server that servers the files in public_html.
  */
 const httpd_config = {
-    port: 80, // Sets the port for the server. Default: 80
-    lang: null, // Sets the language for the documents. When set to null, the default language of the client is used. Default: null
+    port: "8001", // Sets the port for the server. Default: 80
+    lang: null, // Sets the language for the documents. When set to null, the default language of the client is used. Default: undefined
     logging: {
         // Configures, what is logged by the server.
-        remote_lang: true, // Configures, wheter the client language is logged or not. Default: true
-        requested_path: true, // Configures, wheter the requested path is logged or not. Default: true
+        remote_lang: "true", // Configures, wheter the client language is logged or not. Default: true
+        requested_path: "true", // Configures, wheter the requested path is logged or not. Default: true
     },
 };
 

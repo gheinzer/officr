dcc0cc0740c6edceb2e8b7b31dc2e48f1d410d0a
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-08-04 17:05:30 +0200

    Added the tooltip labels for the add-button in todo/index.html in server/lang-specific-content.js

--- server/lang-specific-content.js
+++ server/lang-specific-content.js
@@ -7,11 +7,15 @@ const labels = {
         1: "Fach",
         2: "Beschreibung",
         3: "Fälligkeitsdatum",
+        4: "Aufgabe hinzufügen",
+        5: "Kategorie hinzufügen",
     },
     en: {
         1: "Subject",
         2: "Description",
         3: "Due date",
+        4: "Add task",
+        5: "Add category",
     },
 };
 

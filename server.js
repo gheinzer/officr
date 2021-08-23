68e00f37b5d0d94d511fb48ffc12afca38fa03b7
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-08-23 19:58:13 +0200

    Added import for server/ws.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 require("./server/console");
 console.log("Starting httpd.js...");
-require("./server/httpd"); // This script is responsible serving the files in public_html via http.
+require("./server/httpd"); // This script is responsible for serving the files in public_html via http.
+console.log("Starting ws.js...");
+require("./server/ws"); // This script is responsible for creating a websocket for communication with the front end.

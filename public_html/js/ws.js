21b918c1498570d18520cc30fdd5404283b3884b
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-08-25 19:12:38 +0200

    Added error overlay

--- public_html/js/ws.js
+++ public_html/js/ws.js
@@ -2,3 +2,22 @@ const socket = new WebSocket(
     "ws://" + location.hostname + ":{rawCodeLabel<ws_config.port>}"
 );
 console.log("{rawCodeLabel<publicSessionID>}");
+
+function showErrorOverlay(msg) {
+    document.getElementById("errormessage").innerHTML = msg;
+    document.getElementById("erroroverlay").style.display = "flex";
+}
+
+socket.onmessage = ({ data }) => {
+    const verification_failed = /Verification failed/;
+    if (data.match(verification_failed)) {
+        showErrorOverlay(
+            "Verifizierung fehlgeschlagen. Bitte versuchen Sie es erneut oder <a href='/logout'>melden</a> Sie sich ab und wieder an."
+        );
+        return;
+    }
+};
+socket.onopen = () => {
+    socket.send("INITIALIZE_WITH_SESSION_ID={rawCodeLabel<publicSessionID>}");
+    function getCategories() {}
+};

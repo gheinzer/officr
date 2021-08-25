3117bde492e8057e7abf9a694e659c7c4f15065e
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-08-25 20:35:13 +0200

    Added error handling for the whole back-end

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
-require("./server/console");
-console.log("Starting httpd.js...");
-require("./server/httpd"); // This script is responsible for serving the files in public_html via http.
-console.log("Starting ws.js...");
-require("./server/ws"); // This script is responsible for creating a websocket for communication with the front end.
+var cluster = require("cluster");
+if (cluster.isMaster) {
+    cluster.fork();
+
+    cluster.on("exit", function (worker, code, signal) {
+        cluster.fork();
+    });
+}
+
+if (cluster.isWorker) {
+    start();
+}
+function start() {
+    const officr_logo = `
+                                                                                
+                                                                                
+                                             ,@@@                               
+                       @@@@@@@    @@@@@@@*  @@@@@@@                             
+                     @@@@@@@@*  @@@@@@@@@     #@,                               
+     @@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@    @@@@@       @@@@@@@@     @@@@@ @@@@
+  @@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@@@@@@@    @@@@@    @@@@@@@@@@@@@@  @@@@@@@@@@
+ @@@@@@     @@@@@@   @@@@@      @@@@@        @@@@@   @@@@@            @@@@@@    
+ @@@@@(      @@@@@   @@@@@      @@@@@        @@@@@  #@@@@@            @@@@@     
+  @@@@@@   @@@@@@    @@@@@      @@@@@        @@@@@   @@@@@@.  @@@@@   @@@@@     
+    @@@@@@@@@@@.     @@@@@      @@@@@        @@@@@     @@@@@@@@@@@@   @@@@@     
+                                                                                
+                                                                                
+                                                                                `;
+    console.log("Welcome to officr.");
+    console.log(officr_logo);
+    require("./server/console");
+    console.log("Starting httpd.js...");
+    require("./server/httpd"); // This script is responsible for serving the files in public_html via http.
+    console.log("Starting ws.js...");
+    require("./server/ws"); // This script is responsible for creating a websocket for communication with the front end.
+}

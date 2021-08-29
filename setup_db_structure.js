33ed7e3febb112f318c6233279b5152476e7216d
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-08-29 18:01:43 +0200

    Added stup_db_structure.js

---
+++ setup_db_structure.js
@@ -0,0 +1,14 @@
+const { exec } = require("child_process");
+
+exec("mysql < officr_db_structure.sql", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log("Finished.");
+    console.log(`stdout: ${stdout}`);
+});

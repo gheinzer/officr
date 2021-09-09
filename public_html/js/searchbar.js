6920172a9565067c118e942ed98d0c89b07b9b0e
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-09-09 12:57:19 +0200

    The searchbar will be cleared when you close it.

--- public_html/js/searchbar.js
+++ public_html/js/searchbar.js
@@ -1,10 +1,13 @@
 function hideSearch() {
-  var searchbar = document.getElementById("searchBar");
-  searchbar.style.opacity = 0;
-  searchbar.style.pointerEvents = "none";
+    var searchbar = document.getElementById("searchBar");
+    searchbar.style.opacity = 0;
+    searchbar.style.pointerEvents = "none";
 }
 function showSearch() {
-  var searchbar = document.getElementById("searchBar");
-  searchbar.style.opacity = 1;
-  searchbar.style.pointerEvents = "all";
+    var searchbar = document.getElementById("searchBar");
+    searchbar.style.opacity = 1;
+    searchbar.style.pointerEvents = "all";
+}
+function clearSearchBar() {
+    document.getElementById("search").value = "";
 }

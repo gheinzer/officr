f21635adfa233fce9f51b574fca65827ec575ee1
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-10-10 21:22:06 +0200

    🔂 Added some comments to handleFormInput.js

--- server/handleFormInput.js
+++ server/handleFormInput.js
@@ -2,14 +2,22 @@ const {
     user_verify,
     user_create_session,
     createAccount,
-    getUserByID,
     getUserByName,
 } = require("./user_management");
 
+/**
+ * This handles the input of a POST request.
+ * @param {*} body The request body.
+ * @param {*} req The request object given by the server.
+ * @param {*} res The response object given by the server.
+ */
 function handleFormInput(body, req, res) {
     switch (req.url) {
-        case "/login/submit":
+        case "/login/submit": // This is the the user data is submit from the sign in form.
             var datachunks = body.toString().split("&");
+            /**
+             * This is the data submitted by the user.
+             */
             var data = {};
             datachunks.forEach((element) => {
                 var key = element.split("=")[0];
@@ -17,25 +25,29 @@ function handleFormInput(body, req, res) {
                 data[key] = value;
             });
             if (data.username === undefined || data.password === undefined) {
-                res.statusCode = 400;
+                // Check if all fields are received.
+                res.statusCode = 400; // If not, send 400 Bad Request
                 res.end("Bad request");
                 return;
             }
             const username = data.username.toString();
             const password = data.password.toString();
-            res.statusCode = 302;
+            res.statusCode = 302; // Default status code is not 302 Found.
             user_verify(username, password, function (result) {
                 if (result) {
-                    let publicSessionID = user_create_session(username).public;
+                    // If result is true, the user is verified.
+                    let publicSessionID = user_create_session(username).public; // Creates a session and gets the public session id.
                     if (
                         data.keepmeloggedin === undefined ||
                         data.keepmeloggedin === "off"
                     ) {
+                        // If the keepmeloggedin field is not submitted of =off, set the cookie with the default lifetime.
                         res.setHeader(
                             "Set-Cookie",
                             `officr-user-session-id=${publicSessionID}; HttpOnly; Path=/`
                         );
                     } else {
+                        // If the keepmeloggedin field is checked, set the cookie with increased lifetime.
                         var date = new Date();
                         date.setDate(date.getDate() + 14);
                         res.setHeader(
@@ -43,7 +55,7 @@ function handleFormInput(body, req, res) {
                             `officr-user-session-id=${publicSessionID}; HttpOnly; Path=/; expires=${date.toUTCString()}`
                         );
                     }
-                    res.setHeader("Location", "/todo");
+                    res.setHeader("Location", "/todo"); // Redirect the user to /todo
                     setTimeout(function () {
                         res.end("Authentication successful");
                     }, 500); // This is delayed because of some speed issues with the MySQL Server.
@@ -52,12 +64,15 @@ function handleFormInput(body, req, res) {
                         "Location",
                         "/login?showUsernamePasswordError"
                     );
-                    res.end("Authentication failed.");
+                    res.end("Authentication failed."); // Show an error message if the username or the password is invalid.
                 }
             });
             break;
-        case "/signup/submit":
+        case "/signup/submit": // This is the page for submitting the signup form.
             var datachunks = body.toString().split("&");
+            /**
+             * This is the data that is submitted by the user.
+             */
             var data = {};
             datachunks.forEach((element) => {
                 var key = element.split("=")[0];
@@ -69,9 +84,10 @@ function handleFormInput(body, req, res) {
                 data.password === undefined ||
                 data.email === undefined
             ) {
+                // Check all submitted fields
                 res.statusCode = 400;
                 res.end("Bad request");
-                return;
+                return; // Send a 400 Bad request if fields are invalid.
             }
             const username_to_register = data.username.toString();
             const password_to_register = data.password.toString();
@@ -84,19 +100,19 @@ function handleFormInput(body, req, res) {
                         username_to_register,
                         password_to_register,
                         email
-                    );
+                    ); // Create the new account
 
-                    res.setHeader("Location", "/login");
-                    res.end("Registration succesful.");
+                    res.setHeader("Location", "/login"); // Redirect the user to the login page.
+                    res.end("Registration successful.");
                 } else {
                     res.setHeader("Location", "/signup?alreadyTakenUsername");
-                    res.end("Username already registered");
+                    res.end("Username already registered"); // Redirect the user if the username is already taken.
                 }
             });
             break;
         default:
             res.statusCode = 404;
-            res.end("404 - Not found");
+            res.end("404 - Not found"); // Return a regular 404 if a POST request is sent to a page where it is not valid.
     }
 }
 

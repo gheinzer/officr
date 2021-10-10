ba84c6b7da15bda402f47abaaeb545fcd3264a4f
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-10-10 20:48:22 +0200

    🔂 Added some comments to expired-remover.js

--- server/expired-remover.js
+++ server/expired-remover.js
@@ -1,10 +1,15 @@
+// This script is responsible for removing all expired sessions form the database.
+
 console.log("expired-remover.js was started.");
 const execQuery = require("./mysql");
 
+/**
+ * This deletes all expired sessions from the database.
+ */
 function deleteExpired() {
     execQuery(
         "DELETE FROM usersessions WHERE Expires<" +
-            Math.floor(Date.now() / 1000).toString()
+            Math.floor(Date.now() / 1000).toString() // This calculates the timestamp
     );
 }
 setInterval(deleteExpired, 10000); // Deletes all expired sessions every 10 secons
